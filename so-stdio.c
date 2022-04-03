87011fc9d10c2c02fe3f27aedf22a6dff094218e
maniatro111 <florin.postolache80@gmail.com>
2022-04-03 21:51:18 +0300

    Nice

--- so-stdio.c
+++ so-stdio.c
@@ -6,8 +6,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-struct _so_file
-{
+struct _so_file {
 	/* variable for storing the file descriptor */
 	int fd;
 	/* variable for storing the pid of the child proccess ( if needed) */
@@ -59,24 +58,21 @@ SO_FILE *so_fopen(const char *pathname, const char *mode)
 		aux->fd = open(pathname, O_WRONLY | O_CREAT | O_TRUNC);
 	else if (strcmp(mode, "a") == 0)
 		aux->fd = open(pathname, O_WRONLY | O_APPEND | O_CREAT);
-	else
-	{
+	else {
 		/* if the mode isn't one of the above, return NULL */
 		free(aux);
 		return NULL;
 	}
 
 	/* if the file couldn't be opened free everything and return NULL */
-	if (aux->fd == -1)
-	{
+	if (aux->fd == -1) {
 		free(aux);
 		return NULL;
 	}
 
 	/* reserve memory for the buffer */
 	aux->buffer = (unsigned char *)malloc(4096 * sizeof(unsigned char));
-	if (aux->buffer == NULL)
-	{
+	if (aux->buffer == NULL) {
 		free(aux);
 		return NULL;
 	}
@@ -137,22 +133,19 @@ int so_fflush(SO_FILE *stream)
 {
 	/* check if there is data in the buffer and if the last operation */
 	/* on the structure was a write one */
-	if (stream->start > 0 && stream->last_operation == 2)
-	{
+	if (stream->start > 0 && stream->last_operation == 2) {
 
 		int bytes_to_dump = stream->start;
 		int bytes_wrote;
 		/* while there are still bytes that need to be written, try to
 		 */
 		/* write them in the file */
-		while (bytes_to_dump)
-		{
+		while (bytes_to_dump) {
 			bytes_wrote = write(stream->fd,
-								(stream->buffer) + stream->start -
-									bytes_to_dump,
-								bytes_to_dump);
-			if (bytes_wrote == -1)
-			{
+					    (stream->buffer) + stream->start -
+						bytes_to_dump,
+					    bytes_to_dump);
+			if (bytes_wrote == -1) {
 				stream->start = 0;
 				stream->error = 2;
 				return SO_EOF;
@@ -184,8 +177,7 @@ int so_fflush(SO_FILE *stream)
 int so_fseek(SO_FILE *stream, long offset, int whence)
 {
 	/* if the last operation was a write */
-	if (stream->last_operation == 2)
-	{
+	if (stream->last_operation == 2) {
 		/* write the buffered bytes to the file */
 		int aux = so_fflush(stream);
 		/* return -1 in case of error */
@@ -193,12 +185,11 @@ int so_fseek(SO_FILE *stream, long offset, int whence)
 			return SO_EOF;
 	}
 	/* if the last operation was a read one */
-	else if (stream->last_operation == 1)
-	{
+	else if (stream->last_operation == 1) {
 		/* move the offset of the open channel structure to the last */
 		/* character that was put in the buffer but not read */
 		int aux = lseek(stream->fd, stream->start - stream->len_read,
-						SEEK_CUR);
+				SEEK_CUR);
 		/* return -1 in case of failure */
 		if (aux < -1)
 			return SO_EOF;
@@ -253,12 +244,12 @@ size_t so_fread(void *ptr, size_t size, size_t nmemb, SO_FILE *stream)
 	/* copy bytes to ptr till aux >= nmemb * size or till we encountered an
 	 */
 	/* error */
-	while ((aux < nmemb * size) && stream->error == 0)
-	{
+	while ((aux < nmemb * size) && stream->error == 0) {
 		/* get the result of the so_fgetc function */
 		character_value = (unsigned char)so_fgetc(stream);
 
-		/* if the function finished, but we encountered an error while */
+		/* if the function finished, but we encountered an error while
+		 */
 		/* reading or we reached the end of file, stop copying bytes */
 		if (stream->error == 1 || stream->error == 2)
 			break;
@@ -290,10 +281,9 @@ size_t so_fwrite(const void *ptr, size_t size, size_t nmemb, SO_FILE *stream)
 
 	/* copy bytes to SO_FILE buffer till aux >= nmemb * size or till we */
 	/* encountered an error */
-	while ((aux < nmemb * size) && stream->error == 0)
-	{
+	while ((aux < nmemb * size) && stream->error == 0) {
 		character_value = (unsigned char)so_fputc(
-			(int)(((unsigned char *)ptr)[aux]), stream);
+		    (int)(((unsigned char *)ptr)[aux]), stream);
 		aux++;
 	}
 	/* if there was an error, return 0 */
@@ -314,11 +304,9 @@ size_t so_fwrite(const void *ptr, size_t size, size_t nmemb, SO_FILE *stream)
  */
 int so_fgetc(SO_FILE *stream)
 {
-	/* if the buffer is empty or we read the last character put in buffer,
-	 */
+	/* if the buffer is empty or we read the last character put in buffer */
 	/* we need to read bytes again */
-	if (stream->start == stream->len_read)
-	{
+	if (stream->start == stream->len_read) {
 		int bytes_read;
 
 		stream->start = 0;
@@ -328,21 +316,18 @@ int so_fgetc(SO_FILE *stream)
 		/* mark the last operation as a read one */
 		stream->last_operation = 1;
 		/* if we received 0 bytes, we reached EOF */
-		if (bytes_read == 0)
-		{
+		if (bytes_read == 0) {
 			stream->error = 1;
 			return SO_EOF;
 		}
 		/* if bytes_read is -1, that means there was an error */
-		else if (bytes_read == -1)
-		{
+		else if (bytes_read == -1) {
 			stream->error = 2;
 			return SO_EOF;
 		}
 		/* else actualize the number of bytes read */
 		/* move to the next byte and return the byte before */
-		else
-		{
+		else {
 			stream->len_read = bytes_read;
 			stream->error = 0;
 			stream->start++;
@@ -351,8 +336,7 @@ int so_fgetc(SO_FILE *stream)
 	}
 	/* there are still bytes in buffer */
 	/* move to the next byte and return the byte before */
-	else
-	{
+	else {
 		stream->start++;
 		stream->last_operation = 1;
 		return (int)stream->buffer[stream->start - 1];
@@ -375,26 +359,22 @@ int so_fputc(int c, SO_FILE *stream)
 	/* mark that the last operation was a write one */
 	stream->last_operation = 2;
 	/* if we reached the end of the buffer, flush it in the file */
-	if (stream->start == 4096)
-	{
+	if (stream->start == 4096) {
 		int res = so_fflush(stream);
 
-		if (!res)
-		{
+		if (!res) {
 			memcpy((stream->buffer) + (stream->start), &aux, 1);
 			stream->start++;
 		}
 		/* if there was an error, return an error code and mark that */
 		/* there was an error */
-		else
-		{
+		else {
 			stream->error = 2;
 			return SO_EOF;
 		}
 	}
 	/* else copy the value to the buffer */
-	else
-	{
+	else {
 		memcpy((stream->buffer) + (stream->start), &aux, 1);
 		stream->start++;
 	}
@@ -452,10 +432,10 @@ SO_FILE *so_popen(const char *command, const char *type)
 	/* reserve memory for the read/write buffer */
 	stream->buffer = (unsigned char *)calloc(4096, sizeof(unsigned char));
 
-	/* create the pipe used for communication between the child and parent */
+	/* create the pipe used for communication between the child and parent
+	 */
 	/* proccess */
-	if (pipe(channel))
-	{
+	if (pipe(channel)) {
 		/* if there was a problem in creating the pipe, free the memory
 		 */
 		/* and return NULL */
@@ -475,8 +455,7 @@ SO_FILE *so_popen(const char *command, const char *type)
 	pid = fork();
 
 	/* if the fork failed */
-	if (pid == -1)
-	{
+	if (pid == -1) {
 		/* close the pipe, free the structure and return NULL */
 		close(channel[0]);
 		close(channel[1]);
@@ -485,22 +464,19 @@ SO_FILE *so_popen(const char *command, const char *type)
 		return NULL;
 	}
 	/* if the proccess is the child proccess */
-	else if (pid == 0)
-	{
+	else if (pid == 0) {
 		/* if the type is "r", close the read end of the pipe */
 		/* then redirect the stdout of the child proccess to the write
 		 */
 		/* end of the pipe */
-		if (!strcmp(type, "r"))
-		{
+		if (!strcmp(type, "r")) {
 			close(channel[0]);
 			dup2(channel[1], STDOUT_FILENO);
 		}
 		/* if the type is "w", close the write end of the pipe */
 		/* then redirect the stdin of the child proccess to the read */
 		/* end of the pipe */
-		else
-		{
+		else {
 			close(channel[1]);
 			dup2(channel[0], STDIN_FILENO);
 		}
@@ -508,9 +484,7 @@ SO_FILE *so_popen(const char *command, const char *type)
 		/* return NULL if the exec fails */
 		if (execlp("sh", "sh", "-c", command, NULL))
 			return NULL;
-	}
-	else
-	{
+	} else {
 		/* if the proccess is the parent proccess */
 		stream->pid = pid;
 
